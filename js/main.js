6e758d4b7ae78ff91c7155c784a58c85c217cddb
AtikinXX <atikinxx@gmail.com>
2020-05-15 01:29:48 +0300

    feat: add button scrollup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+/* document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector(".modal");
   const modalBtn = document.querySelectorAll("[data-toggle=modal]");
   const closeBtn = document.querySelector(".modal__close");
@@ -20,4 +20,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
   document.addEventListener("keydown", function (e) {
     if (e.keyCode === 27) modal.classList.remove("modal--visible");
   });
+}); */
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+});
+
+var btn = $('#scroll');
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 1000) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '100');
 });
