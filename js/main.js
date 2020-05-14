567d6387462b5d6253f2bd92e440a3eebd859aa3
AtikinXX <atikinxx@gmail.com>
2020-05-14 15:19:36 +0300

    feat: add window onclick & keyCode in modal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
+document.addEventListener("DOMContentLoaded", function (event) {
+  const modal = document.querySelector(".modal");
+  const modalBtn = document.querySelectorAll("[data-toggle=modal]");
+  const closeBtn = document.querySelector(".modal__close");
   const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+    modal.classList.toggle("modal--visible");
+  };
+
+  modalBtn.forEach((element) => {
+    element.addEventListener("click", switchModal);
   });
-  
-  closeBtn.addEventListener('click', switchModal);
-  
+
+  closeBtn.addEventListener("click", switchModal);
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.classList.remove("modal--visible");
+    }
+  };
+
+  document.addEventListener("keydown", function (e) {
+    if (e.keyCode === 27) modal.classList.remove("modal--visible");
+  });
 });
