870b3d0bb5d25d445e0709e95bc850b96a4f59a2
AtikinXX <atikinxx@gmail.com>
2020-05-24 17:25:47 +0300

    feat: add yandex map API & delite old yandex map

--- js/main.js
+++ js/main.js
@@ -154,6 +154,42 @@ $(document).ready(function () {
   });
   // маска для номера телефона
   $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "Ваш номер телефона:"});
+
+  // создаем yandex map API
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+        center: [47.222531, 39.718705],
+        zoom: 9
+      }, {
+        searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Собственный значок метки',
+        balloonContent: 'Это красивая метка'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'img/marker.png',
+        // Размеры метки.
+        iconImageSize: [32, 32],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+      });
+
+      
+
+    myMap.geoObjects
+      .add(myPlacemark);
+  });
 });
 
 var btn = $('#scroll');
